bcd21363f6ef27cc85b17ab1645c853a0104183f
janhae4 <hlchy11997@gmail.com>
2025-06-08 15:36:51 +0700

    add ml spam detector

---
+++ ml/spam_detector.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app = Flask(__name__)
+
+with open('tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
+
+model = load_model('spam_classifier_model.keras')
+
+max_len = 100 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    message = data.get('message', '')
+
+    sequences = tokenizer.texts_to_sequences([message])
+    padded_seq = pad_sequences(sequences, maxlen=max_len, padding='post', truncating='post')
+
+    pred_prob = model.predict(padded_seq)[0][0]
+    label = 'spam' if pred_prob > 0.5 else 'ham'
+
+    return jsonify({
+        'prediction': label,
+        'probability': float(pred_prob)
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
